fe3f57421f1daddfe452e140e56348283565661b
tangoctrung <tangoctrung12123@gmail.com>
2024-07-21 09:31:33 +0700

    add gtag

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -33,6 +33,23 @@ export default function RootLayout({
           <link rel="preconnect" href="https://fonts.googleapis.com" />
           <link rel="preconnect" href="https://fonts.gstatic.com" />
           <link href="https://fonts.googleapis.com/css2?family=Ubuntu:ital,wght@0,300;0,400;0,500;0,700;1,300;1,400;1,500;1,700&display=swap" rel="stylesheet" />
+          {/* <!-- Google tag (gtag.js) --> */}
+          <script
+            async
+            src={`https://www.googletagmanager.com/gtag/js?id=${process.env.NEXT_PUBLIC_TRACKING_GA}`}
+          ></script>
+          <script
+            dangerouslySetInnerHTML={{
+              __html: `
+              window.dataLayer = window.dataLayer || [];
+            function gtag(){dataLayer.push(arguments);}
+            gtag('js', new Date());
+
+            gtag('config', '${process.env.NEXT_PUBLIC_TRACKING_GA}');
+              `
+            }}
+          />
+
         </head>
         <body className={ubuntu.className} suppressHydrationWarning={true}>
           <AppMain>
