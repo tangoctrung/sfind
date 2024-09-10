ea5bf43fdb440bb67d811d298e2d18f36246f5c7
ngoctrung <tangoctrung12123@gmail.com>
2024-09-10 18:33:37 +0700

    add cronjob

--- src/app/api/ping/route.ts
+++ src/app/api/ping/route.ts
@@ -1,9 +1,17 @@
 import { connectToDB } from "@/servers/mongodb";
 import { convertDataResponse } from "@/servers/utils/convertDataResponse";
+import { sendMessageTelegram } from "@/utils/handleBotTelegram";
 
 export const GET = async () => {
   try {
     connectToDB();
+    fetch("https://sfind.onrender.com/api/ping")
+    .then((res) => {
+      sendMessageTelegram("Ping host sfind.onrender.com success");
+    })
+    .catch((err: any) => {
+      sendMessageTelegram("Ping host sfind.onrender.com fail");
+    });
     return convertDataResponse(200, true, "Server is running...", null);
   } catch (error: any) {
     return convertDataResponse(
