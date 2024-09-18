42c7b40d98ed6004d3b881f06d605d536ab33200
tangoctrung <tangoctrung12123@gmail.com>
2024-09-18 12:20:44 +0700

    delete log

--- src/servers/cronjob/index.js
+++ src/servers/cronjob/index.js
@@ -8,13 +8,13 @@ const job = new CronJob("*/10 * * * *", function () {
 	https
 		.get(URL, (res) => {
 			if (res.statusCode === 200) {
-				sendMessageTelegram("Ping host sfind.onrender.com success");
+				// sendMessageTelegram("Ping host sfind.onrender.com success");
 			} else {
-				sendMessageTelegram("Ping host sfind.onrender.com fail: " + res.statusMessage);
+				// sendMessageTelegram("Ping host sfind.onrender.com fail: " + res.statusMessage);
 			}
 		})
 		.on("error", (e) => {
-			sendMessageTelegram("Ping host sfind.onrender.com fail: " + e?.message);
+			// sendMessageTelegram("Ping host sfind.onrender.com fail: " + e?.message);
 		});
 });
 
