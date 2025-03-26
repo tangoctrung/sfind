b6b3d00db6392e64fbddd889c113e791e6503f91
tangoctrung <tangoctrung12123@gmail.com>
2025-03-26 08:38:51 +0700

    fix middleware

--- src/middleware.ts
+++ src/middleware.ts
@@ -2,17 +2,17 @@ import { NextResponse } from "next/server";
 import type { NextRequest } from "next/server";
 
 export function middleware(request: NextRequest) {
-  // const { pathname } = request.nextUrl
-  // const accessToken = request.cookies.get('accessToken')?.value || ""
-  // // Chưa đăng nhập thì không cho vào private paths
-  // // return NextResponse.next()
-  // if ((pathname !== "/login" && pathname !== "/register") && !accessToken) {
-  //   return NextResponse.redirect(new URL('/login', request.url))
-  // }
-  // // Đăng nhập rồi thì không cho vào login/register nữa
-  // if ((pathname === "/login" || pathname === "/register") && accessToken) {
-  //   return NextResponse.redirect(new URL('/', request.url))
-  // }
+  const { pathname } = request.nextUrl;
+  const accessToken = request.cookies.get("accessToken")?.value || "";
+  // Chưa đăng nhập thì không cho vào private paths
+  // return NextResponse.next()
+  if (pathname !== "/login" && pathname !== "/register" && !accessToken) {
+    return NextResponse.redirect(new URL("/login", request.url));
+  }
+  // Đăng nhập rồi thì không cho vào login/register nữa
+  if ((pathname === "/login" || pathname === "/register") && accessToken) {
+    return NextResponse.redirect(new URL("/", request.url));
+  }
   return NextResponse.next();
 }
 
